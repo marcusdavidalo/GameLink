d1b78d55ee4b4811d5434c3c59272244fc8ca39e
marcusdavidalo <marcusdavidalo@gmail.com>
2023-06-03 12:16:23 +0800

    update GameDetails.js

--- src/pages/GameDetails.js
+++ src/pages/GameDetails.js
@@ -44,6 +44,33 @@ function GameDetails() {
     return true; // Data is valid
   }, []);
 
+  function TruncatedSummary({ summary, maxLength }) {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const handleToggleExpand = () => {
+      setIsExpanded(!isExpanded);
+    };
+
+    if (summary && summary.length > maxLength) {
+      const truncatedSummary = isExpanded
+        ? summary
+        : summary.substring(0, maxLength - 3) + '...';
+
+      return (
+        <React.Fragment>
+          {truncatedSummary}{' '}
+          <button
+            onClick={handleToggleExpand}
+            className="font-semibold text-gray-200"
+          >
+            {isExpanded ? 'Read Less' : 'Read More'}
+          </button>
+        </React.Fragment>
+      );
+    }
+    return summary || '';
+  }
+
   const fetchNewsData = useCallback(() => {
     const cachedNewsData = localStorage.getItem('newsData-' + id);
 
@@ -122,29 +149,12 @@ function GameDetails() {
   console.log('gameData:', gameData);
 
   if (!gameData || !topNews) {
-    return <div>Loading...</div>;
+    return (
+      <div className="fixed h-screen w-screen bg-slate-800/70 backdrop-blur-sm">
+        Loading...
+      </div>
+    );
   }
-
-  // Function to truncate summary and append "Read More" link
-  const truncateSummary = (news, maxLength) => {
-    if (news && news.summary && news.summary.length > maxLength) {
-      const truncatedSummary = news.summary.substring(0, maxLength - 3) + '...';
-      return (
-        <React.Fragment>
-          {truncatedSummary}{' '}
-          <a
-            href={news.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="font-semibold text-gray-200"
-          >
-            Read More
-          </a>
-        </React.Fragment>
-      );
-    }
-    return news ? news.summary : '';
-  };
 
   return (
     <div>
@@ -196,16 +206,18 @@ function GameDetails() {
           <div className="w-full md:w-4/12 mt-5">
             <div className="card rounded-lg bg-gray-800/50 shadow-lg mb-5">
               <div className="card-body">
-                <h5 className="card-title text-gray-200 text-2xl font-bold mb-4 p-5">
+                <h5 className="card-title text-gray-200 text-2xl font-bold p-5">
                   Game Tags
                 </h5>
-                <ul className="list-group">
-                  {gameData.tags.slice(0, 20).map((tag) => (
+                <ul className="flex flex-row flex-wrap list-group bg-gray-800/50 p-5">
+                  {gameData.tags.slice(0, 50).map((tag) => (
                     <li
                       key={tag.id}
-                      className="list-group-item bg-gray-800 text-gray-300 px-5 py-1"
+                      className="list-group-item text-gray-300 mr-2 mb-4"
                     >
-                      {tag.name}
+                      <p className="bg-gray-600/60 px-5 py-2 rounded-full">
+                        {tag.name}
+                      </p>
                     </li>
                   ))}
                 </ul>
@@ -257,7 +269,7 @@ function GameDetails() {
                     slidesPerView: 4,
                   },
                 }}
-                scrollbar={true}
+                scrollbar={{ draggable: true }}
               >
                 {topNews.map((news) => (
                   <SwiperSlide key={news.title} className="pb-10 ">
@@ -270,8 +282,30 @@ function GameDetails() {
                         >
                           <div
                             id="imgcontainer"
-                            className="h-64 max-w-auto overflow-hidden rounded-t-md"
+                            className="relative h-64 max-w-auto overflow-hidden rounded-t-md"
                           >
+                            <span className="absolute top-0 left-0 px-2 py-1 bg-amber-500/70 text-white font-semibold z-20">
+                              {!news.author || news.author === '' ? (
+                                <p>No Author</p>
+                              ) : (
+                                <p>{news.author}</p>
+                              )}
+                            </span>
+                            <span className="absolute bottom-0 right-0 px-2 py-1 bg-blue-700/80 backdrop-blur-sm text-white font-semibold z-20 uppercase">
+                              {!news.author || news.author === '' ? (
+                                <p></p>
+                              ) : (
+                                <a
+                                  href={news.clean_url}
+                                  target="_blank"
+                                  rel="noreferrer"
+                                >
+                                  <p className="hover:scale-105">
+                                    {news.rights}
+                                  </p>
+                                </a>
+                              )}
+                            </span>
                             <img
                               src={news.media}
                               className="card-img-top h-full w-auto object-cover origin-center hover:scale-[1.05]"
@@ -304,7 +338,14 @@ function GameDetails() {
                           </h5>
                         </div>
                         <p className="card-text text-gray-400">
-                          {news.summary ? truncateSummary(news, 100) : ''}
+                          {news.summary ? (
+                            <TruncatedSummary
+                              summary={news.summary}
+                              maxLength={100}
+                            />
+                          ) : (
+                            ''
+                          )}
                         </p>
                       </div>
                     </div>
