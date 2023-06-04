bb9fba18344ace87d7e569e65bc0861575cae26a
Marcus David Alo <45500991+marcusdavidalo@users.noreply.github.com>
2023-06-04 13:29:14 +0800

    Delete Index.js

--- src/pages/Index.js
+++
@@ -1,8 +0,0 @@
-export { default as Home } from './Home';
-export { default as GameDetails } from './GameDetails';
-export { default as NewReleases } from './NewReleases';
-export { default as BestOfYear } from './BestOfYear';
-export { default as AllTimeTop } from './AllTimeTop';
-export { default as About } from './About';
-export { default as NotFound } from './NotFound';
-export { default as Search } from './Search';
