f79e31e6660d02aa80c38d8b01914037fd761b25
marcusdavidalo <marcusdavidalo@gmail.com>
2023-06-10 23:16:32 +0800

    update LoginForm.js

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import jwtDecode from 'jwt-decode';
+import { Transition } from '@headlessui/react';
 
 const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loginStatus, setLoginStatus] = useState('');
   const [username, setUsername] = useState('');
+  const [showMessage, setShowMessage] = useState(false);
 
   useEffect(() => {
     // Check if there is a token in local storage
@@ -14,6 +16,10 @@ const LoginForm = () => {
     if (token) {
       // If there is a token, assume that the user is logged in
       setLoginStatus('Login successful');
+      setShowMessage(true);
+      setTimeout(() => {
+        setShowMessage(false);
+      }, 2000);
 
       // Fetch the username of the logged-in user from the server
       fetchUsername(token);
@@ -57,6 +63,10 @@ const LoginForm = () => {
       if (response.status === 200) {
         // Login successful
         setLoginStatus('Login successful');
+        setShowMessage(true);
+        setTimeout(() => {
+          setShowMessage(false);
+        }, 2000);
 
         // Store the token
         localStorage.setItem('token', response.data.token);
@@ -66,11 +76,19 @@ const LoginForm = () => {
       } else {
         // Login failed
         setLoginStatus(response.data.error);
+        setShowMessage(true);
+        setTimeout(() => {
+          setShowMessage(false);
+        }, 2000);
       }
     } catch (error) {
       // Login failed
       console.log(error);
       setLoginStatus('An error occurred while logging in');
+      setShowMessage(true);
+      setTimeout(() => {
+        setShowMessage(false);
+      }, 2000);
     }
   };
 
@@ -79,56 +97,91 @@ const LoginForm = () => {
       <form
         onSubmit={handleSubmit}
         className="bg-slate-700/50 p-5 w-1/3 m-5 rounded-md"
+        data-aos="zoom-in"
       >
-        <h2 className="text-2xl font-semibold pb-5">Login</h2>
-        {loginStatus && (
-          <p
-            className={`text-center text-white rounded-md my-2 p-2 ${
-              loginStatus === 'Login successful' ? 'bg-green-600' : 'bg-red-600'
-            }`}
-          >
-            {loginStatus}
-          </p>
-        )}
-        {username && (
-          <p className="text-center text-white my-2 p-2 bg-green-600">
-            Logged in as {username}
-          </p>
-        )}
-        <div className="flex flex-col justify-between pb-2">
-          <label htmlFor="email" className="mb-2 font-semibold">
-            Email:
-          </label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Enter your email address"
-            className="border text-gray-200 bg-[rgba(156,163,175,0.5)] border-gray-500 rounded-md py-2 px-4 pr-10 block w-full focus:outline-none focus:ring-slate-400 focus:border-slate-400 dark:text-gray-800 dark:bg-[rgba(255,255,255,0.7)] sm:text-sm"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="flex flex-col justify-between pb-2">
-          <label htmlFor="password" className="mb-2 font-semibold">
-            Password:
-          </label>
-          <input
-            id="password"
-            type="password"
-            placeholder="Enter your password"
-            className="border text-gray-200 bg-[rgba(156,163,175,0.5)] border-gray-500 rounded-md py-2 px-4 pr-10 block w-full focus:outline-none focus:ring-slate-400 focus:border-slate-400 dark:text-gray-800 dark:bg-[rgba(255,255,255,0.7)] sm:text-sm"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            autoComplete="current-password"
-          />
-        </div>
-        <div className="flex justify-center">
-          <button
-            type="submit"
-            className="bg-slate-500/60 px-5 py-2 mb-2 rounded-md"
-          >
-            Login
-          </button>
+        <div data-aos="zoom-out-down" data-aos-duration="500">
+          <div className="pb-5 flex flex-row justify-between items-center content-center box-border">
+            <h2 className="text-3xl font-semibold py-1">Login</h2>
+            <Transition
+              show={showMessage}
+              enter="transition duration-500 ease-out"
+              enterFrom="opacity-0 transform translate-y-[-100%]"
+              enterTo="opacity-100 transform translate-y-50"
+              leave="transition duration-500 ease-in"
+              leaveFrom="opacity-100 transform translate-y-50"
+              leaveTo="opacity-0 transform translate-y-[-100%]"
+              className="w-full px-5"
+            >
+              {(ref) => (
+                <p
+                  ref={ref}
+                  className={`text-center text-white rounded-md py-2 ${
+                    loginStatus === 'Login successful'
+                      ? 'bg-green-600'
+                      : 'bg-red-600'
+                  }`}
+                >
+                  {loginStatus}
+                </p>
+              )}
+            </Transition>
+          </div>
+          {username && (
+            <p
+              className="text-center text-white my-2 p-2 bg-green-600 rounded-md"
+              data-aos="zoom-out"
+            >
+              Logged in as {username}
+            </p>
+          )}
+          <div className="grid grid-cols-1 gap-4">
+            <div className="flex flex-col justify-between pb-2">
+              <label htmlFor="email" className="mb-2 font-semibold">
+                Email:
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter your email address"
+                className="border text-gray-200 bg-[rgba(156,163,175,0.5)] border-gray-500 rounded-md py-2 px-4 pr-10 block w-full focus:outline-none focus:ring-slate-400 focus:border-slate-400 dark:text-gray-800 dark:bg-[rgba(255,255,255,0.7)] sm:text-sm"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col justify-between pb-2">
+              <label htmlFor="password" className="mb-2 font-semibold">
+                Password:
+              </label>
+              <input
+                id="password"
+                type="password"
+                placeholder="Enter your password"
+                className="border text-gray-200 bg-[rgba(156,163,175,0.5)] border-gray-500 rounded-md py-2 px-4 pr-10 block w-full focus:outline-none focus:ring-slate-400 focus:border-slate-400 dark:text-gray-800 dark:bg-[rgba(255,255,255,0.7)] sm:text-sm"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                autoComplete="current-password"
+              />
+            </div>
+            <div className="flex justify-center">
+              <button
+                type="submit"
+                className="bg-slate-500/60 px-5 py-2 mb-2 rounded-md"
+              >
+                Login
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center mt-5">
+            <button className="bg-red-600 px-5 py-2 rounded-md mb-4">
+              Continue with Google
+            </button>
+            <button className="bg-blue-400 px-5 py-2 rounded-md mb-4">
+              Continue with Twitter
+            </button>
+            <button className="bg-blue-800 px-5 py-2 rounded-md mb-4">
+              Continue with Facebook
+            </button>
+          </div>
         </div>
       </form>
     </>
